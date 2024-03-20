d04f8d8aca678f921da6d291ee0a04685ac00469
Toloza  Leonel <leotoloza6@gmail.com>
2024-03-20 18:22:49 -0300

    ya lee el input

--- app/src/main/java/com/leotoloza/traductor/MainActivity.java
+++ app/src/main/java/com/leotoloza/traductor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.leotoloza.traductor;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.leotoloza.traductor.databinding.ActivityMainBinding;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+      String palabra=binding.palabra.getText().toString();
+      binding.btnTraducir.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              String palabra=binding.palabra.getText().toString();
+              Log.d("salida",palabra);
+          }
+      });
+
     }
+
+    public void generarLista(){
+        Map<String, String> map = new HashMap<>();
+        map.put("arbol", "tree");
+        map.put("casa", "house");
+        map.put("auto", "car");
+        map.put("gato", "cat");
+        map.put("perro", "dog");
+        map.put("caballo", "horse");
+        map.put("vaca", "cow");
+    }
+
+
+
+
+
 }
