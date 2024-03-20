921fa3f1355ccf429b6caee072fb5fa4fa60d5e1
Mauro Corvalan <105509270+Maurocorvalan@users.noreply.github.com>
2024-03-20 20:48:06 -0300

    EnviarPalabra
    se envia la palabra al viewmodel

---
+++ app/src/main/java/com/leotoloza/traductor/TraduccionViewModel.java
@@ -0,0 +1,14 @@
+package com.leotoloza.traductor;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class TraduccionViewModel extends AndroidViewModel {
+
+    public TraduccionViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+}
