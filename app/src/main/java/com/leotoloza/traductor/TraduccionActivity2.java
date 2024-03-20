7fdebf9d1add7a0bed9450ed6c735621fd404d49
Toloza  Leonel <leotoloza6@gmail.com>
2024-03-20 17:52:52 -0300

    subiendo app

---
+++ app/src/main/java/com/leotoloza/traductor/TraduccionActivity2.java
@@ -0,0 +1,19 @@
+package com.leotoloza.traductor;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TraduccionActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traduccion2);
+
+    }
+}
