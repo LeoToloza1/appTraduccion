350d715e4b57b4941b1bf27815ca60e162eb1f1c
Mauro Corvalan <105509270+Maurocorvalan@users.noreply.github.com>
2024-03-21 11:15:15 -0300

    Traduccion
    Se agregan las imagenes

--- app/src/main/java/com/leotoloza/traductor/TraduccionActivity2.java
+++ app/src/main/java/com/leotoloza/traductor/TraduccionActivity2.java
@@ -3,6 +3,9 @@ package com.leotoloza.traductor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,14 +17,26 @@ import androidx.lifecycle.ViewModelProvider;
 public class TraduccionActivity2 extends AppCompatActivity {
     private TraduccionViewModel mv;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traduccion2);
         mv = new ViewModelProvider(this).get(TraduccionViewModel.class);
+
        Intent intent = getIntent();
        Palabra result= mv.verificaPalabra(intent);
+
+       //Obtener referencias elementos
+       TextView traduccionTextView = findViewById(R.id.traduccion);
+       ImageView imagenTradImageView = findViewById(R.id.imagenTrad);
+       Button btnAtrasButton = findViewById(R.id.btnAtras);
+
+        //Muestra la palabra traducida y la imagen
+        traduccionTextView.setText(result.getTraducccion());
+        imagenTradImageView.setImageResource(result.getImg());
+
+        // Agregar un Listener al botón para cerrar la actividad
+        btnAtrasButton.setOnClickListener(view -> finish());
        Log.d("salida", "Salida-> "  + result.getTraducccion() + " "+ "img-> " + result.getImg());
        // si l apalbra buscdano esta llegara un palabr aError,  siempre se imprime lo que llegue
 
