bd8533201bca0dbb2795abddb408d0822586a1d7
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-03-21 00:27:29 -0300

    Update TraduccionActivity2.java

--- app/src/main/java/com/leotoloza/traductor/TraduccionActivity2.java
+++ app/src/main/java/com/leotoloza/traductor/TraduccionActivity2.java
@@ -19,22 +19,21 @@ public class TraduccionActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traduccion2);
-
         mv = new ViewModelProvider(this).get(TraduccionViewModel.class);
-
-
        Intent intent = getIntent();
-
        Palabra result= mv.verificaPalabra(intent);
+       Log.d("salida", "Salida-> "  + result.getTraducccion() + "--"+ "img-> " + result.getImg());
+       // si l apalbra buscdano esta llegara un palabr aError,  siempre se imprime lo que llegue
 
-        if ("Error".equals(result.getTraducccion())) {
+       /* if ("Error".equals(result.getTraducccion())) {
             Log.d("salida", "Error");
         }
         else
         {
             Log.d("salida", "ok");
-
         }
+
+        */
     }
 
 }
